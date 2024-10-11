5ba40c8dcd9d4f39d82127e58779c6278e78b94f
Alurall <andre.pedrocv@gmail.com>
2024-10-11 21:01:18 +0200

    add description with the first message

--- src/main.js
+++ src/main.js
@@ -10,9 +10,18 @@ client.on("ready", () => {
     console.log(`${client.user.username} connected`);
 });
 
+var event;
+function resetEvent() {
+    event = {
+        eventId: "",
+        message: ""
+    }
+}
+
 client.on('threadCreate', async (thread) => {
     // Make sure it's only the forum id we want
     if (thread.type == ChannelType.PublicThread && thread.parentId == process.env.FORUM_ID) {
+        resetEvent();
         // Try to get the Dungeon banner based on the thread name
         const dungeon = Object.values(dungeons).find(d =>
             d.alias.some(alias => thread.name.toLowerCase().includes(alias.toLowerCase()))
@@ -37,8 +46,34 @@ client.on('threadCreate', async (thread) => {
             })
         });
         const data = await response.json();
+        event.eventId = data.event.id;
+        udpateEvent();
         console.log(`Created event >${data.event.title}< by ${data.event.leaderName}`);
     }
 })
+
+client.on("messageCreate", async (message) => {
+    if (message.channel.parentId == process.env.FORUM_ID && message.channel.messageCount == 1) {
+        event.message = message;
+        udpateEvent()
+    }
+});
+
+async function udpateEvent() {
+    if (event.eventId && event.message) {
+        const url = `https://raid-helper.dev/api/v2/events/${event.eventId}`
+        const response = await fetch(url, {
+            method: 'PATCH',
+            headers: {
+                'Authorization': process.env.RAIDHELPER_TOKEN,
+                'Content-Type': 'application/json; charset=utf-8'
+            },
+            body: JSON.stringify({
+                description: event.message.content
+            })
+        });
+        resetEvent();
+    }
+}
 
 client.login(process.env.DISCORD_TOKEN);
