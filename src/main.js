5ba656dba07a29604443078515ca70c2c2ec8a74
Alurall <andre.pedrocv@gmail.com>
2024-10-12 15:36:44 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const { Client, GatewayIntentBits, ChannelType } = require("discord.js");
+const { setTimeout } = require('timers/promises');
 const { dungeons } = require("./dungeons.js").default;
 require('log-timestamp');
 
@@ -13,19 +14,20 @@ client.on("ready", () => {
 client.on('threadCreate', async (thread) => {
     // Make sure it's only the forum id we want
     if (thread.parentId == process.env.FORUM_ID) {
-        console.log(thread);
+        console.log("thread -> ", thread);
     }
 })
 
 client.on("messageCreate", async (message) => {
     if (message.channel.parentId == process.env.FORUM_ID && message.channel.messageCount == 1) {
-        console.log(message);
+        console.log("message -> ", message);
         let thread = message.channel;
         // Try to get the Dungeon banner based on the thread name
         const dungeon = Object.values(dungeons).find(d =>
             d.alias.some(alias => thread.name.toLowerCase().includes(alias.toLowerCase()))
         );
-        // Call raid helper api to create a new event
+        // Call raid helper api to create a new event and wait for the thread to exist
+        await setTimeout(3000);
         const url = `https://raid-helper.dev/api/v2/servers/${thread.guildId}/channels/${thread.id}/event`
         const response = await fetch(url, {
             method: 'POST',
@@ -46,7 +48,7 @@ client.on("messageCreate", async (message) => {
             })
         });
         const data = await response.json();
-        console.log(data); // debug
+        console.log("api -> ", data); // debug
         console.log(`Created event >${data.event.title}< by ${data.event.leaderName}`);
     }
 });
