5733befe94b526aedb5c4be3b84ce0405d9ded51
Alurall <andre.pedrocv@gmail.com>
2024-10-12 14:45:19 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,18 +10,9 @@ client.on("ready", () => {
     console.log(`${client.user.username} connected`);
 });
 
-var event;
-function resetEvent() {
-    event = {
-        eventId: "",
-        message: ""
-    }
-}
-
-client.on('threadCreate', async (thread) => {
-    // Make sure it's only the forum id we want
-    if (thread.type == ChannelType.PublicThread && thread.parentId == process.env.FORUM_ID) {
-        resetEvent();
+client.on("messageCreate", async (message) => {
+    if (message.channel.parentId == process.env.FORUM_ID && message.channel.messageCount == 1) {
+        let thread = message.channel;
         // Try to get the Dungeon banner based on the thread name
         const dungeon = Object.values(dungeons).find(d =>
             d.alias.some(alias => thread.name.toLowerCase().includes(alias.toLowerCase()))
@@ -38,6 +29,7 @@ client.on('threadCreate', async (thread) => {
                 leaderId: thread.ownerId,
                 templateId: 2, // wow classic
                 title: thread.name,
+                description: message.content,
                 advancedSettings: {
                     image: dungeon ? dungeon.banner : "",
                     alt_names: true,
@@ -46,34 +38,9 @@ client.on('threadCreate', async (thread) => {
             })
         });
         const data = await response.json();
-        event.eventId = data.event.id;
-        udpateEvent();
+        console.log(data); // debug
         console.log(`Created event >${data.event.title}< by ${data.event.leaderName}`);
     }
-})
-
-client.on("messageCreate", async (message) => {
-    if (message.channel.parentId == process.env.FORUM_ID && message.channel.messageCount == 1) {
-        event.message = message;
-        udpateEvent()
-    }
 });
-
-async function udpateEvent() {
-    if (event.eventId && event.message) {
-        const url = `https://raid-helper.dev/api/v2/events/${event.eventId}`
-        const response = await fetch(url, {
-            method: 'PATCH',
-            headers: {
-                'Authorization': process.env.RAIDHELPER_TOKEN,
-                'Content-Type': 'application/json; charset=utf-8'
-            },
-            body: JSON.stringify({
-                description: event.message.content
-            })
-        });
-        resetEvent();
-    }
-}
 
 client.login(process.env.DISCORD_TOKEN);
